16dbdd50d2317bb210970255a829460111490cd5
Joseph Olarte <josephsantiago2345@gmail.com>
2024-07-08 12:23:49 -0500

    Agregando las paginas que tendra el proyecto con algunos estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenidos a Nombre del Restaurante</h1>
+      <p>Disfruta de la mejor comida en un ambiente acogedor.</p>
+    </div>
+  );
+};
+
+export default Home;
